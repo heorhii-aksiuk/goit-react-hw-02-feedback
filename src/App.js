8aec2c534237d63aa2fd6881c295f91288dd249b
Heorhii Aksiuk <aksiuk.heorhii@gmail.com>
2021-12-03 21:17:21 +0200

    Create base HTML for feedback part

--- src/App.js
+++ src/App.js
@@ -3,8 +3,29 @@ import { Component } from "react";
 import Container from "./components/Container/Container";
 
 class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
   render() {
-    return <Container></Container>;
+    const { good, neutral, bad } = this.state;
+
+    return (
+      <Container>
+        <h2>Please leave your feedback</h2>
+        <button type="button">Good</button>
+        <button type="button">Neutral</button>
+        <button type="button">Bad</button>
+        <h2>Statistic</h2>
+        <ul>
+          <li>Good: {good}</li>
+          <li>Neutral: {neutral}</li>
+          <li>Bad: {bad}</li>
+        </ul>
+      </Container>
+    );
   }
 }
 
